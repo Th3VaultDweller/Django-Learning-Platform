7a12b63e7e6665ed92e38430f5f5acbc53ecb353
Th3VaultDweller <aztec242@yandex.ru>
2023-10-06 15:21:13 +0300

    Added Course model

--- education/courses/models.py
+++ education/courses/models.py
@@ -12,6 +12,21 @@ class Subject(models.Model):
 
     class Meta:
         ordering = ["title"]
-    
+
     def __str__(self):
         return self.title
+
+
+class Course(models.Model):
+    """A representation of a course that a student is in"""
+
+    owner = models.ForeignKey(
+        User, related_name="courses_created", on_delete=models.CASCADE
+    )
+    subject = models.ForeignKey(
+        Subject, related_name="courses", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
