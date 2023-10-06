92c1a83accd6fcf1c3cdce9b947c1a52dfe07835
Th3VaultDweller <aztec242@yandex.ru>
2023-10-06 18:48:22 +0300

    Typo fix

--- education/courses/admin.py
+++ education/courses/admin.py
@@ -8,18 +8,17 @@ from .models import Course, Module, Subject
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ["title", "slug"]
-    prepopulated_fields = {"slug", ("title",)}
+    prepopulated_fields = {"slug": ("title",)}
 
 
-@admin.register(Module)
-class ModuleInLine(admin.StackedInline):
+class ModuleInline(admin.StackedInline):
     model = Module
 
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ["title", "subject", "created"]
-    list_filter = ["subject", "created"]
+    list_filter = ["created", "subject"]
     search_fields = ["title", "overview"]
     prepopulated_fields = {"slug": ("title",)}
-    inlines = [ModuleInLine]
+    inlines = [ModuleInline]
