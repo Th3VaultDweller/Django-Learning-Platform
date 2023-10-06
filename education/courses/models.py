e962b0df9335b5b9e01ec0f32fb6e5113e4e926c
Th3VaultDweller <aztec242@yandex.ru>
2023-10-06 15:12:02 +0300

    Added a Subject model

--- education/courses/models.py
+++ education/courses/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Subject(models.Model):
+    """A representation of a learning subject"""
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["title"]
+    
+    def __str__(self):
+        return self.title
