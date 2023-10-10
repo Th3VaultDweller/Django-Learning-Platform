2a0af742c18aebd651a1c599cfbfa906f7210f1b
Th3VaultDweller <aztec242@yandex.ru>
2023-10-10 23:06:09 +0300

    Typo fix

--- education/courses/urls.py
+++ education/courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-url_patterns: [
+urlpatterns: [
     path("mine/", views.ManageCourseListView.as_view(), name="manage_course_list"),
     path("create/", views.CourseCreateView.as_view(), name="course_create"),
     path("<pk>/edit/", views.CourseUpdateView.as_view(), name="course_edit"),
