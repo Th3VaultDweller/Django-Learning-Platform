5ce8b5e9cd3a24fb8e717466ab764900685fa823
Th3VaultDweller <aztec242@yandex.ru>
2023-10-10 22:46:43 +0300

    Imported new mixins

--- education/courses/views.py
+++ education/courses/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
