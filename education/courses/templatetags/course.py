9dece8e647363e7a29f4275a8cc698d579da995e
Th3VaultDweller <aztec242@yandex.ru>
2023-10-15 23:19:26 +0300

    Added comment

--- education/courses/templatetags/course.py
+++ education/courses/templatetags/course.py
@@ -2,9 +2,12 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def model_name(obj):
     """Template filter"""
+    # In templates it is used as object|model_name
+    # to get a model name for an object
     try:
         return obj._meta.model_name
     except AttributeError:
