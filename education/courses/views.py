79d90df39943899864226501d6552da997431d61
Th3VaultDweller <aztec242@yandex.ru>
2023-10-19 15:00:11 +0300

    Added ModuleOrderView

--- education/courses/views.py
+++ education/courses/views.py
@@ -160,3 +160,10 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({"module": module})
+
+
+class ModuleOrderView(CsrfExempMixin, JsonRequestResponceMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_responce({"saved": "OK"})
