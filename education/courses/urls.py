c87dea3ef8fe144945a359fb1128dab85f003ba0
Th3VaultDweller <aztec242@yandex.ru>
2023-10-19 15:08:09 +0300

    Added urls

--- education/courses/urls.py
+++ education/courses/urls.py
@@ -3,10 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("mine/", views.ManageCourseListView.as_view(), name="manage_course_list"),
-    path("create/", views.CourseCreateView.as_view(), name="course_create"),
-    path("<pk>/edit/", views.CourseUpdateView.as_view(), name="course_edit"),
-    path("<pk>/delete/", views.CourseDeleteView.as_view(), name="course_delete"),
+    path("mine/", 
+         views.ManageCourseListView.as_view(), 
+         name="manage_course_list"),
+    path("create/", 
+         views.CourseCreateView.as_view(), 
+         name="course_create"),
+    path("<pk>/edit/", 
+         views.CourseUpdateView.as_view(), 
+         name="course_edit"),
+    path("<pk>/delete/", 
+         views.CourseDeleteView.as_view(), 
+         name="course_delete"),
     path(
         "<pk>/module/",
         views.CourseModuleUpdateView.as_view(),
@@ -32,4 +40,10 @@ urlpatterns = [
         views.ModuleContentListView.as_view(),
         name="module_content_list",
     ),
+    path("module/order/", 
+         views.ModuleOrderView.as_view(), 
+         name="module_order"),
+    path("content/order", 
+         views.ContentOrderView.as_view(), 
+         name="content_order"),
 ]
