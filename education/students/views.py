bb34c532af56c704b47d12bacd5e8d36112d7b29
Th3VaultDweller <aztec242@yandex.ru>
2023-10-19 19:42:33 +0300

    New import

--- education/students/views.py
+++ education/students/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
