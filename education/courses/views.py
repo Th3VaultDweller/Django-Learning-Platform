5d8d320a5dd221f249a442462a3b14b07c46bb48
Th3VaultDweller <aztec242@yandex.ru>
2023-10-20 18:01:39 +0300

    Extended CourseDetailView

--- education/courses/views.py
+++ education/courses/views.py
@@ -3,7 +3,8 @@ from typing import Any
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 from django import http
 from django.apps import apps
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.db.models import Count
 from django.forms.models import modelform_factory
 from django.shortcuts import get_object_or_404, redirect, render
@@ -207,3 +208,9 @@ class CourseDetailView(DetailView):
 
     model = Course
     template_name = "courses/course/detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["enroll_form"] = CourseEnrollForm(
+            initial={"course": self.object})
+        return context
