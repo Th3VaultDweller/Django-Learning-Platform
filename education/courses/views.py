830d694c5bd832a28548b073b983344cc2a7d61f
Th3VaultDweller <aztec242@yandex.ru>
2023-10-20 17:56:34 +0300

    New import

--- education/courses/views.py
+++ education/courses/views.py
@@ -12,6 +12,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from students.forms import CourseEnrollForm
 
 from .forms import ModuleFormSet
 from .models import Content, Course, Module, Subject
@@ -206,4 +207,3 @@ class CourseDetailView(DetailView):
 
     model = Course
     template_name = "courses/course/detail.html"
-    
