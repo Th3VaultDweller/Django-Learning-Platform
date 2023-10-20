dce4e15b03e35ab7ed41616cba46e34c4fff7553
Th3VaultDweller <aztec242@yandex.ru>
2023-10-20 17:48:46 +0300

    Added StudentEnrollCourseView

--- education/students/views.py
+++ education/students/views.py
@@ -29,3 +29,18 @@ class StudentRegistrationView(CreateView):
         user = authenticate(username=cd["username"], password=cd["password1"])
         login(self.request, user)
         return result
+
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    """Works with the enrolled students"""
+
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data["course"]
+        self.course.students.add(self.request.user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("student_course_detail", args=[self.course.id])
