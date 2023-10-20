7f49967af5e3e2d5c6104da3cba20c63c74a2bde
Th3VaultDweller <aztec242@yandex.ru>
2023-10-20 19:03:49 +0300

    Login redirect tweaks

--- education/education/settings.py
+++ education/education/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from decouple import config
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +131,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/course/mine'
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
